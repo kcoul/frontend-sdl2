7ef08da44f65dc547c0af040d4d6ab69ea830df5
kcoul <kieran.coulter@gmail.com>
2023-12-22 11:39:48 -0800

    Add debug builds of projectM, and all available textures

--- cmake/FindprojectM4.cmake
+++ cmake/FindprojectM4.cmake
@@ -1,12 +1,21 @@
 add_library(libprojectM::projectM SHARED IMPORTED)
 add_library(libprojectM::playlist SHARED IMPORTED)
 
-message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Release-iphoneos/libprojectM-4.dylib")
-message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Release-iphoneos/libprojectM-4-playlist.dylib")
+# TODO: Make into Multi-config generator expression
+
+#message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Release-iphoneos/libprojectM-4.dylib")
+#message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Release-iphoneos/libprojectM-4-playlist.dylib")
+
+message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Debug-iphoneos/libprojectM-4d.dylib")
+message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Debug-iphoneos/libprojectM-4-playlistd.dylib")
+
+#set_property(TARGET libprojectM::projectM PROPERTY
+#        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Release-iphoneos/libprojectM-4.dylib")
+#set_property(TARGET libprojectM::playlist PROPERTY
+#        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Release-iphoneos/libprojectM-4-playlist.dylib")
 
 set_property(TARGET libprojectM::projectM PROPERTY
-        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Release-iphoneos/libprojectM-4.dylib")
-
+        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4/iOS-arm64/Debug-iphoneos/libprojectM-4d.dylib")
 set_property(TARGET libprojectM::playlist PROPERTY
-        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Release-iphoneos/libprojectM-4-playlist.dylib")
+        IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/projectM4-playlist/iOS-arm64/Debug-iphoneos/libprojectM-4-playlistd.dylib")
 
