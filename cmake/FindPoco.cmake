d3f78e7322e0e34ec66c858786089adf86fec386
kcoul <kieran.coulter@gmail.com>
2023-12-21 23:59:06 -0800

    iOS build links

--- cmake/FindPoco.cmake
+++ cmake/FindPoco.cmake
@@ -1,13 +1,13 @@
 add_library(Poco::Util SHARED IMPORTED)
 
-message("${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoUtil.dylib")
+message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoUtil.dylib")
 
 set_property(TARGET Poco::Util PROPERTY
         IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoUtil.dylib")
 
 add_library(Poco::Foundation SHARED IMPORTED)
 
-message("${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoFoundation.dylib")
+message("Using ${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoFoundation.dylib")
 
 set_property(TARGET Poco::Foundation PROPERTY
         IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/libs/poco/iOS-arm64/libPocoFoundation.dylib")
